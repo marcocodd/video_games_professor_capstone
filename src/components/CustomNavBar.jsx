5a1fa874b98d19d3fa2cd26f4d28772e91aebf22
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-05-03 17:27:04 +0200

    added search bar and store link

--- src/components/CustomNavBar.jsx
+++ src/components/CustomNavBar.jsx
@@ -10,40 +10,56 @@ import {
 } from "react-bootstrap";
 import logo from "../assets/logo.jpg";
 
-const CustomNavBar = () => (
- <Navbar expand="lg" className="bg-navbar rounded position-sticky">
-  <Container fluid>
-   <Navbar.Brand href="#home">
-    <img className="rounded-circle" width={50} src={logo} alt="logo image" />
-   </Navbar.Brand>
-   <Navbar.Toggle aria-controls="basic-navbar-nav" />
-   <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="m-auto">
-     <Nav.Link className="me-2" href="#home">
-      Reviews
-     </Nav.Link>
-     <Nav.Link className="me-2" href="#link">
-      Games
-     </Nav.Link>
-     <NavDropdown
-      className="text-white"
-      title="Platforms"
-      id="basic-nav-dropdown"
-     >
-      <NavDropdown.Item href="#action/3.1">Playstation</NavDropdown.Item>
-      <NavDropdown.Item href="#action/3.2">Xbox</NavDropdown.Item>
-      <NavDropdown.Item href="#action/3.3">Nintendo Switch</NavDropdown.Item>
-      <NavDropdown.Item href="#action/3.4">Pc</NavDropdown.Item>
-     </NavDropdown>
-    </Nav>
-    <img
-     className="rounded-circle"
-     src="https://placebear.com/50/50"
-     alt="Avatar"
-    />
-   </Navbar.Collapse>
-  </Container>
- </Navbar>
-);
+const CustomNavBar = () => {
+ return (
+  <Navbar expand="lg" className="bg-navbar rounded position-sticky">
+   <Container fluid>
+    <Navbar.Brand href="#home">
+     <img className="rounded-circle" width={50} src={logo} alt="logo image" />
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+     <Nav className="m-auto">
+      <Nav.Link className="me-2" href="#home">
+       Reviews
+      </Nav.Link>
+      <Nav.Link className="me-2" href="#link">
+       Games
+      </Nav.Link>
 
+      <Form className="d-flex">
+       <Form.Control
+        type="search"
+        placeholder="Search"
+        className="me-2"
+        aria-label="Search"
+       />
+      </Form>
+      <NavDropdown
+       className="text-white"
+       title="Platforms"
+       id="basic-nav-dropdown"
+      >
+       <NavDropdown.Item href="#action/3.1">Playstation</NavDropdown.Item>
+       <NavDropdown.Item href="#action/3.2">Xbox</NavDropdown.Item>
+       <NavDropdown.Item href="#action/3.3">Nintendo Switch</NavDropdown.Item>
+       <NavDropdown.Item href="#action/3.4">Pc</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link className="me-2" href="#link">
+       Store
+      </Nav.Link>
+     </Nav>
+
+     <Button className="bg-transparent border-0 rounded-circle p-0">
+      <img
+       className="rounded-circle me-2"
+       src="https://placebear.com/50/50"
+       alt="Avatar"
+      />
+     </Button>
+    </Navbar.Collapse>
+   </Container>
+  </Navbar>
+ );
+};
 export default CustomNavBar;
